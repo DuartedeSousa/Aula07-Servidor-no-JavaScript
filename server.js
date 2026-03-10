1bd16d2589434d8b6cf21888937f8bfa3d379bdb
Duarte <henrique.d.sousa@aluno.senai.br>
2026-03-10 09:43:54 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+//Importar o módulo HTTP nativo no Node.js
+//Esse módulo permite criar servidores web
+const http = require("http");
+
+//Criarum servidor http
+//Função que será executada toda vez que alguém acessar o servidor
+const server = httpcreateServer((req, res) => { //A função é executada por si mesma
+    
+})
